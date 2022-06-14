7581300df0452ec39cf34b8fa222d38404454aa7
Mazdak <nazemi.works@gmail.com>
2022-06-15 01:21:17 +0200

    csv : everything works fine

--- src/components/CSVContainer/CSVContainer.styled.ts
+++ src/components/CSVContainer/CSVContainer.styled.ts
@@ -1,1 +1,9 @@
-export {}
+import styled from "styled-components";
+
+
+export const CSVContainterStyle = styled.div`
+  padding: 1rem 2rem;
+  max-height: 96%;
+  overflow-y: auto;
+  overflow-x: scroll;
+`
--- src/components/CSVContainer/CSVContainer.tsx
+++ src/components/CSVContainer/CSVContainer.tsx
@@ -1,37 +1,56 @@
-import  { FC } from 'react'
-import CSVReader from 'react-csv-reader'
+import { FC } from 'react'
+import ReactJson from 'react-json-view'
 
 // ---
 
-import { useApp } from '../../context';
+import { useApp } from '../../context'
+import { CSVContainterStyle } from './CSVContainer.styled'
 
 // ---
 
 interface IProps {
-  table : boolean
+	table: boolean
 }
 
 // ---
 
-export const CSVContainer : FC<IProps> = ({ table }) => {
-  const { fileCsv, message } = useApp()
+export const CSVContainer: FC<IProps> = ({ table }) => {
+	const { fileCsv, message } = useApp()
 
-  const renderer = (info: any) => {
-    if(table) {
-      return <div>i am table for csv {info.name}</div>
-    } else {
-      return <div>i am JSON for csv {info.name}</div>
-    }
-  }
+	const renderer = (info: any) => {
+		if (table) {
+			return (
+				<table>
+					<thead>
+						<tr>
+							{info[0].map((item: any, idx: number) => (
+								<th key={idx}>{item}</th>
+							))}
+						</tr>
+					</thead>
+					<tbody>
+						{info.slice(1).map((item: any, idx: number) => (
+							<tr key={idx}>
+								{item.map((item: any, idx: number) => (
+									<td key={idx}>{item.replace(/[^\x00-\x7F]/g, "")}</td>
+								))}
+							</tr>
+						))}
+					</tbody>
+				</table>
+			)
+		} else {
+			return (
+				<div>
+					<ReactJson src={info} />
+				</div>
+			)
+		}
+	}
 
-
-  return (
-    <div>
-      {fileCsv  ? <>
-        
-        {renderer(fileCsv)}
-      </>
-      : <div>{message}</div>}
-    </div>
-  )
+	return (
+		<CSVContainterStyle>
+			{fileCsv ? <>{renderer(fileCsv)}</> : <div>{message}</div>}
+		</CSVContainterStyle>
+	)
 }
